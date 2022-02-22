8ff8a88f394a996bb3d0958c432543ced46f8e6d
VoxIP <jos@voxip.nl>
2022-02-23 00:06:26 +0100

    Update run.sh

--- ssh_tunnel/run.sh
+++ ssh_tunnel/run.sh
@@ -1,58 +1,47 @@
-#!/usr/bin/env bashio
+#!/usr/bin/with-contenv bashio
 set -e
 
-# https://github.com/hassio-addons/bashio
+CONFIG_PATH=/data/options.json
+#KEY_PATH=/data/ssh_keys
 
-bashio::log.info "Public key:"
+HOSTNAME=$(jq --raw-output ".hostname" $CONFIG_PATH)
+SSH_PORT=$(jq --raw-output ".ssh_port" $CONFIG_PATH)
+USERNAME=$(jq --raw-output ".username" $CONFIG_PATH)
+KEY=$(jq --raw-output ".key" $KEY)
 
-cat /root/.ssh/id_rsa.pub
+REMOTE_FORWARDING=$(jq --raw-output ".remote_forwarding[]" $CONFIG_PATH)
 
-BASE="ssh -o StrictHostKeyChecking=no"
-TUN="-o ExitOnForwardFailure=yes -o ServerAliveInterval=30 -N"
+OTHER_SSH_OPTIONS=$(jq --raw-output ".other_ssh_options" $CONFIG_PATH)
 
-if bashio::config.true 'tunnel_http'; then
-    # -R remote_socket:host:hostport
-    # Specifies that connections to the given TCP port or Unix socket on the
-    # remote (server) host are to be forwarded to the local side.
-    TUN="${TUN} -R 80:localhost:80"
+#
+
+if [ -z "$HOSTNAME" ]; then
+  bashio::log.error "Please set 'hostname' in your config to the address of your remote server"
+  exit 1
 fi
 
-if bashio::config.true 'tunnel_https'; then
-    TUN="${TUN} -R 443:localhost:443"
+#
+
+COMMAND="/usr/bin/autossh "\
+" -M 0 -N "\
+"-o ServerAliveInterval=30 "\
+"-o ServerAliveCountMax=3 "\
+"-o StrictHostKeyChecking=no "\
+"-o ExitOnForwardFailure=yes "\
+"-p ${SSH_PORT} -t -t "\
+"-i /dev/stdin "\
+"${USERNAME}@${HOSTNAME}"
+
+if [ ! -z "${REMOTE_FORWARDING}" ]; then
+  while read -r LINE; do
+    COMMAND="${COMMAND} -R ${LINE}"
+  done <<< "${REMOTE_FORWARDING}"
 fi
 
-if ! bashio::config.equals 'socks_port' 0; then
-    # -D [bind_address:]port
-    # Currently the SOCKS4 and SOCKS5 protocols are supported, and ssh will act
-    # as a SOCKS server.
-    TUN="${TUN} -D *:$(bashio::config 'socks_port')"
-fi
+COMMAND="${COMMAND} ${OTHER_SSH_OPTIONS}"
 
-if ! bashio::config.is_empty 'advanced'; then
-    TUN="${TUN} $(bashio::config 'advanced')"
-fi
+bashio::log.info "Executing command: ${COMMAND}"
+/usr/bin/autossh -V
 
-SRV="$(bashio::config 'ssh_user')@$(bashio::config 'ssh_host')"
-
-if ! bashio::config.equals 'ssh_port' 22; then
-    SRV="-p $(bashio::config 'ssh_port') ${SRV}"
-fi
-
-SRV="-o StrictHostKeyChecking=no ${SRV}"
-
-set +e
-
-while true
-do
-    if ! bashio::config.is_empty 'before'; then
-        CMD="${BASE} ${SRV} $(bashio::config 'before')"
-        bashio::log.info "[ $(date +'%m-%d-%Y') ] run: ${CMD}"
-        eval $CMD
-    fi
-
-    CMD="${BASE} ${TUN} ${SRV}"
-    bashio::log.info "[ $(date +'%m-%d-%Y') ] run tunnel: ${CMD}"
-    eval $CMD
-
-    sleep 30
-done
+# Execute
+exec echo ${KEY} | ${COMMAND}
